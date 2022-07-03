cb94f9c89895455a439b19fa11b023902c247a41
Shamik Rahman <shamikrah@gmail.com>
2022-07-03 20:53:36 +1000

    Change styling and add working modal window

--- script.js
+++ script.js
@@ -3,10 +3,30 @@ let myLibrary = [];
 
 // Declare and add listeners to DOM elements
 const cardContainer = document.querySelector(".card-container");
-const form = document.querySelector("#add-book");
-form.addEventListener("submit", addBookToLibrary);
-const newBookBtn = document.querySelector("#new-book");
+const overlay = document.querySelector(".modal-overlay");
+const formContainer = document.querySelector(".form-container");
+const form = document.querySelector(".form");
+form.addEventListener("submit", test);
+const newBookBtn = document.querySelector(".new-book");
 newBookBtn.addEventListener("click", displayModal);
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
+const cancelBtn = document.querySelector(".cancel-btn");
+cancelBtn.addEventListener("click", closeModal);
+
+// Open modal window
+function displayModal() {
+    overlay.classList.add("active");
+    formContainer.classList.add("active");
+}
+
+// Close modal window
+function closeModal() {
+    overlay.classList.remove("active");
+    formContainer.classList.remove("active");
+}
 
 // Book object with constructor
 function Book(title, author, pages, isRead) {
@@ -24,19 +44,35 @@ Book.prototype.changeReadStatus = function () {
     this.isRead = !(this.isRead);
 }
 
+function test() {
+    console.log("test");
+}
+
 // Add book to myLibrary
-function addBookToLibrary(book) {
+function addBookToLibrary() {
+    console.log("lol");
+    let currTitle = title.textContent;
+    console.log(currTitle);
+    let currAuthor = author.textContent;
+    console.log(currAuthor);
+    let currPages = pages.textContent;
+    console.log(currPages);
+    let currRead = read.textContent;
+    console.log(currRead);
+    let book = new Book(currTitle, currAuthor, currPages, currRead);
     myLibrary.push(book);
     displayBooks();
 }
 
-// Manually add example book to myLibrary for testing
+// Manually add example books to myLibrary for testing
 const exampleBook = new Book("Harry Potter", "JK Rowling", 100, true);
 myLibrary.push(exampleBook);
 const exampleBook2 = new Book("James and the Small Peach", "Roll Daal", 300, false);
 myLibrary.push(exampleBook2);
 const exampleBook3 = new Book("Goosebumps", "That Guy", 50, false);
 myLibrary.push(exampleBook3);
+const exampleBook4 = new Book("Goosebumps", "That Guy", 50, false);
+myLibrary.push(exampleBook4);
 displayBooks();
 
 // Remove book from myLibrary
@@ -53,7 +89,7 @@ function displayBooks() {
     for (let book of myLibrary) {
         let card = document.createElement("div");
         for (let key in book) {
-            console.log(book[key]);
+            //console.log(book[key]);
             if ((key == "title")) {
                 card.classList.add("card");
                 let cardItem = document.createElement("div");
@@ -103,9 +139,4 @@ function displayBooks() {
         cardContainer.appendChild(card);
     }
 }
-
-function displayModal() {
-    
-}
-
 
